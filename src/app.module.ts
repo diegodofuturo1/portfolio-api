8da1a33690ee0d8816e5abf595ac349a5c2e6419
Diego Heleno <diegodofuturo1@gmail.com>
2022-07-28 13:00:09 -0300

    fixed: prod build

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,9 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { PortfolioModule } from './endpoint/portfolio/portfolio.module';
 
 const typeormConfig: TypeOrmModuleOptions = {
-  type: process.env.DB_TYPE,
+  type: process.env.DB_TYPE as 'mysql' | 'mariadb',
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
